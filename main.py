ace23956cd5311f5ec142f2fb9b1f71f38e15a27
Kevin Changoluisa <46755578+KevinChangoluisa@users.noreply.github.com>
2023-05-30 08:34:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,11 @@ def index():
 @app.route('/get_imgWRF', methods=['POST'])
 def get_imgWRF():
     url = request.json.get('url')
-    print("La url es: ",url)
     username = os.environ.get('USERNAME')
     password = os.environ.get('PASSWORD')
     response = requests.get(url, auth=(username, password))
     response.raise_for_status()
-    base64_data = base64.b64encode(response.content).decode('utf-8')
-    return base64_data
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    return base64.b64encode(response.content).decode('utf-8')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
