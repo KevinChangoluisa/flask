2c3c069f50904b9797daf3701b999507f8e4d496
Kevin Changoluisa <46755578+KevinChangoluisa@users.noreply.github.com>
2023-05-29 21:33:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from flask import Flask, jsonify
 import requests
 import base64
-from configparser import ConfigParser
 import os
-
-
-config = ConfigParser()
-config.read('config.ini')
-wrf_config = config['WRF']
 
 app = Flask(__name__)
 
@@ -19,12 +13,15 @@ def index():
 @app.route('/get_imgWRF', methods=['POST'])
 def get_imgWRF():
     url = request.json.get('url')
-    username = wrf_config.get('USERNAME')
-    password = wrf_config.get('PASSWORD')
+    username = os.environ.get('USERNAME')
+    password = os.environ.get('PASSWORD')
     response = requests.get(url, auth=(username, password))
     response.raise_for_status()
     base64_data = base64.b64encode(response.content).decode('utf-8')
     return base64_data
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == '__main__':
